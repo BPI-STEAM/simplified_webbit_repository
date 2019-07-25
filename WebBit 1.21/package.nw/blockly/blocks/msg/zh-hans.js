3280070b8439369d943b17c8c620826f58fe5cce
Juwan <junhuanchen@qq.com>
2019-07-25 17:25:18 +0800

    数位 改 数字

--- WebBit 1.21/package.nw/blockly/blocks/msg/zh-hans.js
+++ WebBit 1.21/package.nw/blockly/blocks/msg/zh-hans.js
@@ -1248,9 +1248,9 @@ Blockly.Msg.BIT_BOARD_HELPURL = Blockly.Msg.TUTORIAL_URL + "board/board.html";
 
 Blockly.Msg.WEBDUINO_PIN_READ = "读取引脚";
 Blockly.Msg.WEBDUINO_PIN_ANALOG_WRITE = "模拟输出 ( PWM ) 至引脚";
-Blockly.Msg.WEBDUINO_PIN_DIGITAL_WRITE = "数位输出至引脚";
+Blockly.Msg.WEBDUINO_PIN_DIGITAL_WRITE = "数字输出至引脚";
 Blockly.Msg.WEBDUINO_PIN_VALUE = "数值";
-Blockly.Msg.WEBDUINO_PIN_DIN = "数位输入";;
+Blockly.Msg.WEBDUINO_PIN_DIN = "数字输入";;
 Blockly.Msg.WEBDUINO_PIN_AIN = "模拟输入";
 
 // Web:Bit - Matrix
--- WebBit 1.21/package.nw/blockly/custom-blocks/pin/blockly/msg/blocks/zh-hans.js
+++ WebBit 1.21/package.nw/blockly/custom-blocks/pin/blockly/msg/blocks/zh-hans.js
@@ -1,6 +1,6 @@
 Blockly.Msg.WEBDUINO_PIN_READ = "读取引脚";
 Blockly.Msg.WEBDUINO_PIN_ANALOG_WRITE = "模拟输出 ( PWM ) 至引脚";
-Blockly.Msg.WEBDUINO_PIN_DIGITAL_WRITE = "数位输出至引脚";
+Blockly.Msg.WEBDUINO_PIN_DIGITAL_WRITE = "数字输出至引脚";
 Blockly.Msg.WEBDUINO_PIN_VALUE = "数值";
-Blockly.Msg.WEBDUINO_PIN_DIN = "数位输入";;
+Blockly.Msg.WEBDUINO_PIN_DIN = "数字输入";;
 Blockly.Msg.WEBDUINO_PIN_AIN = "模拟输入";
